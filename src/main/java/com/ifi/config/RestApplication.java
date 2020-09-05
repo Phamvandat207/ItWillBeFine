10e0cf219dd399c688e1c5b8ab1465bc12c9c30a
Duc <vanduc2514@gmail.com>
2020-09-05 11:40:39 +0700

    Add dependency, configure for DB Connection and JPA.

--- src/main/java/com/ifi/config/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.ifi.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {
-}
