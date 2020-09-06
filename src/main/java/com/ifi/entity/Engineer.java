d8a347a5d40fde111029d4944e4389d243417d18
Duc <vanduc2514@gmail.com>
2020-09-07 02:51:32 +0700

    Add super constructor

--- src/main/java/com/ifi/entity/Engineer.java
+++ src/main/java/com/ifi/entity/Engineer.java
@@ -1,28 +1,42 @@
 package com.ifi.entity;
 
-import lombok.AllArgsConstructor;
+import com.ifi.util.constants.Gender;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity(name = "engineer")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Engineer extends Employee {
     //Lương tháng
     @Basic
     @Column(name = "monthly_wage", precision = 10, scale = 9)
-    private BigDecimal hourlyRating;
+    @NonNull
+    private BigDecimal monthlyWage;
 
     //Phụ cấp
     @Basic
     @Column(name = "allowance", precision = 8, scale = 7)
+    @NonNull
     private BigDecimal allowance;
+
+    public Engineer(@NonNull String name,
+                    @NonNull Gender gender,
+                    @NonNull Date dateOfBirth,
+                    @NonNull Date joinedDate,
+                    @NonNull BigDecimal monthlyWage,
+                    @NonNull BigDecimal allowance) {
+        super(name, gender, dateOfBirth, joinedDate);
+        this.monthlyWage = monthlyWage;
+        this.allowance = allowance;
+    }
 }
--- src/main/java/com/ifi/entity/Worker.java
+++ src/main/java/com/ifi/entity/Worker.java
@@ -1,23 +1,34 @@
 package com.ifi.entity;
 
-import lombok.AllArgsConstructor;
+import com.ifi.util.constants.Gender;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Entity(name = "worker")
 @Data
 @EqualsAndHashCode(callSuper = true)
-@AllArgsConstructor
 @NoArgsConstructor
 public class Worker extends Employee {
     //Lương 1 công
     @Basic
     @Column(name = "hourly_rating", precision = 6, scale = 5)
+    @NonNull
     private BigDecimal hourlyRating;
+
+    public Worker(@NonNull String name,
+                  @NonNull Gender gender,
+                  @NonNull Date dateOfBirth,
+                  @NonNull Date joinedDate,
+                  @NonNull BigDecimal hourlyRating) {
+        super(name, gender, dateOfBirth, joinedDate);
+        this.hourlyRating = hourlyRating;
+    }
 }
