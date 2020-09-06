976f1470bb95392afedfe5a855f38391cf02b5a2
Duc <vanduc2514@gmail.com>
2020-09-06 17:32:31 +0700

    Add Servlet Listener Remove Bean Scope

--- src/main/java/com/ifi/util/container/BeanResources.java
+++ src/main/java/com/ifi/util/container/BeanResources.java
@@ -1,13 +1,10 @@
 package com.ifi.util.container;
 
-
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@ApplicationScoped
-public class ApplicationContext {
+public class BeanResources {
     @Produces
     @PersistenceContext
     private EntityManager entityManager;
