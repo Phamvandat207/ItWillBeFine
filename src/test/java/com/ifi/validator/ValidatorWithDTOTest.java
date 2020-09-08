1510bef38ff6a529f65c5890584ec7d07554b458
Duc <vanduc2514@gmail.com>
2020-09-09 01:58:27 +0700

    Add Test Validator

--- src/test/java/com/ifi/validator/ValidatorWithDTOTest.java
+++ src/test/java/com/ifi/validator/ValidatorWithDTOTest.java
@@ -4,20 +4,33 @@ import com.ifi.constants.Gender;
 import com.ifi.dto.EmployeeDTO;
 import com.ifi.dto.EngineerDTO;
 import com.ifi.dto.WorkerDTO;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.opentest4j.AssertionFailedError;
+import org.junit.jupiter.api.TestInstance;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ValidatorWithDTOTest {
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setupValidatorInstance() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
     UUID expectedId = UUID.randomUUID();
     String expectedName = "valid_name";
     Gender expectedGender = Gender.MALE;
@@ -28,8 +41,8 @@ public class ValidatorWithDTOTest {
     Date expectedJoinedDate = Date.from(Instant.now());
 
     @Test
-    @DisplayName("Should Create Employee with Valid Data")
-    void should_create_employee_with_valid_data() {
+    @DisplayName("Should validate Employee with Valid Data")
+    void should_validate_employee_with_valid_data() {
         EmployeeDTO employeeDTO = new EmployeeDTO(
                 expectedId,
                 expectedName,
@@ -37,18 +50,13 @@ public class ValidatorWithDTOTest {
                 expectedDob,
                 expectedJoinedDate
         );
-
-        assertNotNull(employeeDTO);
-        assertEquals(expectedId, employeeDTO.getId());
-        assertEquals(expectedName, employeeDTO.getName());
-        assertEquals(expectedGender, employeeDTO.getGender());
-        assertEquals(expectedDob, employeeDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, employeeDTO.getJoinedDate());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Employee with Null id")
-    void should_create_employee_with_null_id() {
+    @DisplayName("Should validate Employee with Null id")
+    void should_validate_employee_with_null_id() {
         EmployeeDTO employeeDTO = new EmployeeDTO(
                 null,
                 expectedName,
@@ -56,21 +64,15 @@ public class ValidatorWithDTOTest {
                 expectedDob,
                 expectedJoinedDate
         );
-
-        assertNotNull(employeeDTO);
-        assertNull(employeeDTO.getId());
-        assertEquals(expectedName, employeeDTO.getName());
-        assertEquals(expectedGender, employeeDTO.getGender());
-        assertEquals(expectedDob, employeeDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, employeeDTO.getJoinedDate());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Engineer with Valid data")
-    void should_create_engineer_with_valid_data() {
+    @DisplayName("Should validate Engineer with Valid data")
+    void should_validate_engineer_with_valid_data() {
         BigDecimal expectedMonthlyWage = BigDecimal.valueOf(0.08);
         BigDecimal expectedAllowance = BigDecimal.valueOf(0.003);
-
         EngineerDTO engineerDTO = new EngineerDTO(
                 expectedId,
                 expectedName,
@@ -80,23 +82,15 @@ public class ValidatorWithDTOTest {
                 expectedMonthlyWage,
                 expectedAllowance
         );
-
-        assertNotNull(engineerDTO);
-        assertEquals(expectedId, engineerDTO.getId());
-        assertEquals(expectedName, engineerDTO.getName());
-        assertEquals(expectedGender, engineerDTO.getGender());
-        assertEquals(expectedDob, engineerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, engineerDTO.getJoinedDate());
-        assertEquals(expectedMonthlyWage, engineerDTO.getEngineerMonthlyWage());
-        assertEquals(expectedAllowance, engineerDTO.getEngineerAllowance());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(engineerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Engineer with null id")
-    void should_create_engineer_with_null_id() {
+    @DisplayName("Should validate Engineer with null id")
+    void should_validate_engineer_with_null_id() {
         BigDecimal expectedMonthlyWage = BigDecimal.valueOf(0.08);
         BigDecimal expectedAllowance = BigDecimal.valueOf(0.003);
-
         EngineerDTO engineerDTO = new EngineerDTO(
                 null,
                 expectedName,
@@ -106,20 +100,13 @@ public class ValidatorWithDTOTest {
                 expectedMonthlyWage,
                 expectedAllowance
         );
-
-        assertNotNull(engineerDTO);
-        assertNull(engineerDTO.getId());
-        assertEquals(expectedName, engineerDTO.getName());
-        assertEquals(expectedGender, engineerDTO.getGender());
-        assertEquals(expectedDob, engineerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, engineerDTO.getJoinedDate());
-        assertEquals(expectedMonthlyWage, engineerDTO.getEngineerMonthlyWage());
-        assertEquals(expectedAllowance, engineerDTO.getEngineerAllowance());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(engineerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Engineer with null id and null wage")
-    void should_create_engineer_with_null_id_and_wage() {
+    @DisplayName("Should validate Engineer with null id and null wage")
+    void should_validate_engineer_with_null_id_and_wage() {
         EngineerDTO engineerDTO = new EngineerDTO(
                 null,
                 expectedName,
@@ -129,22 +116,14 @@ public class ValidatorWithDTOTest {
                 null,
                 null
         );
-
-        assertNotNull(engineerDTO);
-        assertNull(engineerDTO.getId());
-        assertEquals(expectedName, engineerDTO.getName());
-        assertEquals(expectedGender, engineerDTO.getGender());
-        assertEquals(expectedDob, engineerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, engineerDTO.getJoinedDate());
-        assertNull(engineerDTO.getEngineerMonthlyWage());
-        assertNull(engineerDTO.getEngineerAllowance());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(engineerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Worker with Valid data")
-    void should_create_worker_with_valid_data() {
+    @DisplayName("Should validate Worker with Valid data")
+    void should_validate_worker_with_valid_data() {
         BigDecimal expectedHourlyRating = BigDecimal.valueOf(0.03);
-
         WorkerDTO workerDTO = new WorkerDTO(
                 expectedId,
                 expectedName,
@@ -153,21 +132,14 @@ public class ValidatorWithDTOTest {
                 expectedJoinedDate,
                 expectedHourlyRating
         );
-
-        assertNotNull(workerDTO);
-        assertEquals(expectedId, workerDTO.getId());
-        assertEquals(expectedName, workerDTO.getName());
-        assertEquals(expectedGender, workerDTO.getGender());
-        assertEquals(expectedDob, workerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, workerDTO.getJoinedDate());
-        assertEquals(expectedHourlyRating, workerDTO.getWorkerHourlyRating());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(workerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Worker with null id")
-    void should_create_worker_with_null_id() {
+    @DisplayName("Should validate Worker with null id")
+    void should_validate_worker_with_null_id() {
         BigDecimal expectedHourlyRating = BigDecimal.valueOf(0.03);
-
         WorkerDTO workerDTO = new WorkerDTO(
                 null,
                 expectedName,
@@ -176,19 +148,13 @@ public class ValidatorWithDTOTest {
                 expectedJoinedDate,
                 expectedHourlyRating
         );
-
-        assertNotNull(workerDTO);
-        assertNull(workerDTO.getId());
-        assertEquals(expectedName, workerDTO.getName());
-        assertEquals(expectedGender, workerDTO.getGender());
-        assertEquals(expectedDob, workerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, workerDTO.getJoinedDate());
-        assertEquals(expectedHourlyRating, workerDTO.getWorkerHourlyRating());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(workerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Create Worker with null id and null hourly rating")
-    void should_create_worker_with_null_id_and_rating() {
+    @DisplayName("Should validate Worker with null id and null hourly rating")
+    void should_validate_worker_with_null_id_and_rating() {
         WorkerDTO workerDTO = new WorkerDTO(
                 null,
                 expectedName,
@@ -197,19 +163,120 @@ public class ValidatorWithDTOTest {
                 expectedJoinedDate,
                 null
         );
-
-        assertNotNull(workerDTO);
-        assertNull(workerDTO.getId());
-        assertEquals(expectedName, workerDTO.getName());
-        assertEquals(expectedGender, workerDTO.getGender());
-        assertEquals(expectedDob, workerDTO.getDateOfBirth());
-        assertEquals(expectedJoinedDate, workerDTO.getJoinedDate());
-        assertNull(workerDTO.getWorkerHourlyRating());
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(workerDTO);
+        assertEquals(0, violations.size());
     }
 
     @Test
-    @DisplayName("Should Not create Employee with default constructor")
-    void should_not_create_employee() {
+    @DisplayName("Should Not validate Employee with null name")
+    void should_not_validate_employee_null_name() {
+        EmployeeDTO employeeDTO = new EmployeeDTO(
+                expectedId,
+                null,
+                expectedGender,
+                expectedDob,
+                expectedJoinedDate);
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should Not validate Employee with invalid name")
+    void should_not_validate_employee_invalid_name() {
+        EmployeeDTO employeeDTO = new EmployeeDTO(
+                expectedId,
+                "a",
+                expectedGender,
+                expectedDob,
+                expectedJoinedDate
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should Not validate Employee with null gender")
+    void should_not_validate_employee_null_gender() {
+        EmployeeDTO employeeDTO = new EmployeeDTO(
+                expectedId,
+                expectedName,
+                null,
+                expectedDob,
+                expectedJoinedDate
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should Not validate Employee with null joined date")
+    void should_not_validate_employee_null_joinedDate() {
+        EmployeeDTO employeeDTO = new EmployeeDTO(
+                expectedId,
+                expectedName,
+                expectedGender,
+                expectedDob,
+                null
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(employeeDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should not validate Engineer with negative wage")
+    void should_not_validate_engineer_negative_wage() {
+        BigDecimal expectedMonthlyWage = BigDecimal.valueOf(-10.08);
+        BigDecimal expectedAllowance = BigDecimal.valueOf(0.003);
+        EngineerDTO engineerDTO = new EngineerDTO(
+                expectedId,
+                expectedName,
+                expectedGender,
+                expectedDob,
+                expectedJoinedDate,
+                expectedMonthlyWage,
+                expectedAllowance
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(engineerDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should not validate Engineer with negative allowance")
+    void should_not_validate_engineer_negative_allowance() {
+        BigDecimal expectedMonthlyWage = BigDecimal.valueOf(0.08);
+        BigDecimal expectedAllowance = BigDecimal.valueOf(-12.003);
+        EngineerDTO engineerDTO = new EngineerDTO(
+                expectedId,
+                expectedName,
+                expectedGender,
+                expectedDob,
+                expectedJoinedDate,
+                expectedMonthlyWage,
+                expectedAllowance
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(engineerDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should not validate Worker with negative rating")
+    void should_not_validate_engineer_negative_rating() {
+        BigDecimal expectedRating = BigDecimal.valueOf(-12.003);
+        WorkerDTO workerDTO = new WorkerDTO(
+                null,
+                expectedName,
+                expectedGender,
+                expectedDob,
+                expectedJoinedDate,
+                expectedRating
+        );
+        Set<ConstraintViolation<EmployeeDTO>> violations = validator.validate(workerDTO);
+        assertNotEquals(0, violations.size());
+    }
+
+    @Test
+    @DisplayName("Should Not validate Employee with default constructor")
+    void should_not_validate_employee() {
         Class<EmployeeDTO> clazz = EmployeeDTO.class;
         assertThrows(InstantiationException.class, () -> {
             EmployeeDTO employeeDTO = clazz.newInstance();
@@ -218,8 +285,8 @@ public class ValidatorWithDTOTest {
     }
 
     @Test
-    @DisplayName("Should Not create Engineer with default constructor")
-    void should_not_create_engineer() {
+    @DisplayName("Should Not validate Engineer with default constructor")
+    void should_not_validate_engineer() {
         Class<EngineerDTO> clazz = EngineerDTO.class;
         assertThrows(InstantiationException.class, () -> {
             EngineerDTO engineerDTO = clazz.newInstance();
@@ -228,108 +295,12 @@ public class ValidatorWithDTOTest {
     }
 
     @Test
-    @DisplayName("Should Not create Worker with default constructor")
-    void should_not_create_worker() {
+    @DisplayName("Should Not validate Worker with default constructor")
+    void should_not_validate_worker() {
         Class<WorkerDTO> clazz = WorkerDTO.class;
         assertThrows(InstantiationException.class, () -> {
             WorkerDTO workerDTO = clazz.newInstance();
             assertNull(workerDTO);
         });
-    }
-
-    @Test
-    @DisplayName("Should Not create Employee with null name")
-    void should_not_create_employee_null_name() {
-        assertThrows(AssertionFailedError.class, () -> {
-            EmployeeDTO employeeDTO = new EmployeeDTO(
-                    expectedId,
-                    null,
-                    expectedGender,
-                    expectedDob,
-                    expectedJoinedDate
-            );
-            assertNull(employeeDTO);
-        });
-    }
-
-    @Test
-    @DisplayName("Should Not create Employee with invalid name")
-    void should_not_create_employee_invalid_name() {
-        assertThrows(AssertionFailedError.class, () -> {
-            EmployeeDTO employeeDTO = new EmployeeDTO(
-                    expectedId,
-                    "a",
-                    expectedGender,
-                    expectedDob,
-                    expectedJoinedDate
-            );
-            assertNull(employeeDTO);
-        });
-    }
-
-    @Test
-    @DisplayName("Should Not create Employee with null gender")
-    void should_not_create_employee_null_gender() {
-        assertThrows(AssertionFailedError.class, () -> {
-            EmployeeDTO employeeDTO = new EmployeeDTO(
-                    expectedId,
-                    expectedName,
-                    null,
-                    expectedDob,
-                    expectedJoinedDate
-            );
-            assertNull(employeeDTO);
-        });
-    }
-
-    @Test
-    @DisplayName("Should Not create Employee with null joined date")
-    void should_not_create_employee_null_joinedDate() {
-        assertThrows(AssertionFailedError.class, () -> {
-            EmployeeDTO employeeDTO = new EmployeeDTO(
-                    expectedId,
-                    expectedName,
-                    expectedGender,
-                    expectedDob,
-                    null
-            );
-            assertNull(employeeDTO);
-        });
-    }
-
-    @Test
-    @DisplayName("Should not create Engineer with negative wage")
-    void should_not_create_engineer_negative_wage() {
-        BigDecimal expectedMonthlyWage = BigDecimal.valueOf(-369547.08);
-        BigDecimal expectedAllowance = BigDecimal.valueOf(0.003);
-
-        if (expectedAllowance.doubleValue() > 0) {
-            System.out.println("haha");
-        }
-
-        EngineerDTO engineerDTO1 = new EngineerDTO(
-                expectedId,
-                expectedName,
-                expectedGender,
-                expectedDob,
-                expectedJoinedDate,
-                expectedMonthlyWage,
-                expectedAllowance
-        );
-        System.out.println(engineerDTO1);
-
-        assertThrows(AssertionFailedError.class, () -> {
-            EngineerDTO engineerDTO = new EngineerDTO(
-                    expectedId,
-                    expectedName,
-                    expectedGender,
-                    expectedDob,
-                    expectedJoinedDate,
-                    expectedMonthlyWage,
-                    expectedAllowance
-            );
-            assertNull(engineerDTO);
-        });
-
     }
 }
