7f102b4dd7e2c8b1b6603deaa42eb14f725eaa6a
phamvandat207 <phamvandat207@gmail.com>
2020-09-08 14:18:09 +0700

    add entities DTO (employeeDTO, engineerDTO, workerDTO)

--- src/main/java/com/ifi/dto/EmployeeDTO.java
+++ src/main/java/com/ifi/dto/EmployeeDTO.java
@@ -1,4 +1,40 @@
 package com.ifi.dto;
 
-public class EmployeeDTO {
+
+import com.ifi.constants.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+import javax.persistence.Basic;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Value
+@AllArgsConstructor
+public class EmployeeDTO implements Serializable {
+
+    private UUID id;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @NotNull
+    @Basic
+    private Gender gender;
+
+    @NotNull
+    @Basic
+    private Date dateOfBirth;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+
 }
--- src/main/java/com/ifi/dto/WorkerDTO.java
+++ src/main/java/com/ifi/dto/WorkerDTO.java
@@ -1,4 +1,41 @@
 package com.ifi.dto;
 
+import com.ifi.constants.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+import javax.persistence.Basic;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+
+@Value
+@AllArgsConstructor
 public class WorkerDTO {
+
+    private UUID id;
+
+    @NotNull
+    @Size(max = 50)
+    private String name;
+
+    @NotNull
+    @Basic
+    private Gender gender;
+
+    @NotNull
+    @Basic
+    private Date dateOfBirth;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date joinedDate;
+
+    @NotNull
+    @Basic
+    private BigDecimal hourlyRating;
 }
