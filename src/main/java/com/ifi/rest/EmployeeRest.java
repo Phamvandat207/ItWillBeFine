16687527244c1d19f2663cba630cafa2798cf312
Duc <vanduc2514@gmail.com>
2020-09-10 00:10:06 +0700

    Add Validation for Rest

--- src/main/java/com/ifi/rest/EmployeeRest.java
+++ src/main/java/com/ifi/rest/EmployeeRest.java
@@ -5,6 +5,7 @@ import com.ifi.dto.EmployeeDTO;
 import com.ifi.service.employee.EmployeeService;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -25,12 +26,12 @@ public class EmployeeRest {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addNewEmployee(EmployeeDTO employeeDTO) {
+    public Response addNewEmployee(@Valid EmployeeDTO employeeDTO) {
         EmployeeDTO employeeAdded;
         try {
             employeeAdded = employeeService.createNewEmployee(employeeDTO);
         } catch (EmployeeSaveException e) {
-            return Response.status(502, "bad request").build();
+            return Response.status(400, "Cannot add employee with pre-defined id").build();
         }
         return Response.ok(employeeAdded).build();
     }
