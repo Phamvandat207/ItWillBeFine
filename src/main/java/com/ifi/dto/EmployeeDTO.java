e2266d51050524b214f07e7354e4292482c57e39
Mr. Duc <vanduc2514@gmail.com>
2020-09-09 13:23:56 +0700

    Support Json Deserialize Immutable object

--- src/main/java/com/ifi/dto/EmployeeDTO.java
+++ src/main/java/com/ifi/dto/EmployeeDTO.java
@@ -1,6 +1,9 @@
 package com.ifi.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ifi.constants.Gender;
 import lombok.Builder;
 import lombok.Data;
@@ -13,22 +16,33 @@ import java.util.UUID;
 @Data
 @Builder
 public class EmployeeDTO {
-
+    @JsonProperty("employee_id")
     final UUID id;
 
     @NotNull(message = "name cannot be null")
     @Size(min = 2, max = 50, message = "name must between 2 and 50 characters")
+    @JsonProperty("employee_name")
     final String name;
 
     @NotNull(message = "gender cannot be null")
+    @JsonProperty("gender")
     final Gender gender;
 
+    @JsonFormat(locale = "en", shape = JsonFormat.Shape.STRING, pattern = "dd-MM-YYYY")
+    @JsonProperty("date_of_birth")
     final Date dateOfBirth;
 
     @NotNull(message = "joined date cannot be null")
+    @JsonFormat(locale = "en", shape = JsonFormat.Shape.STRING, pattern = "dd-MM-YYYY")
+    @JsonProperty("joined_date")
     final Date joinedDate;
 
-    public EmployeeDTO(UUID id, @NotNull @Size(min = 2, max = 50) String name, @NotNull Gender gender, @NotNull Date dateOfBirth, @NotNull Date joinedDate) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public EmployeeDTO(@JsonProperty("employee_id") UUID id,
+                       @JsonProperty("employee_name") @NotNull @Size(min = 2, max = 50) String name,
+                       @JsonProperty("gender") @NotNull Gender gender,
+                       @JsonProperty("date_of_birth") @NotNull Date dateOfBirth,
+                       @JsonProperty("joined_date") @NotNull Date joinedDate) {
         this.id = id;
         this.name = name;
         this.gender = gender;
